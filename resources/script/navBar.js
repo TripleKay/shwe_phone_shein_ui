7ef0d0047f1dc7a89a8729f3edfb41c5a486afd7
TripleKay <triplekay.k3@gmail.com>
2022-11-22 11:17:48 +0630

    to share

--- resources/script/navBar.js
+++ resources/script/navBar.js
@@ -10,6 +10,14 @@ let screenHeight = $(window).height();
             $(".site-nav").removeClass("site-nav-scroll");
         }
     });
+
+    $(".input").focusin(function () {
+        $(this).find("span").animate({ opacity: "0" }, 200);
+      });
+      
+      $(".input").focusout(function () {
+        $(this).find("span").animate({ opacity: "1" }, 300);
+      });
 /* -------------------------------end nav slider-------------------------------------   */
 
 
